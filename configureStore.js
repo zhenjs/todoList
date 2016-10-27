27abed1f8bcb909c0873d3ef5dfbd8f54664a4c1
anzhen <779729176@qq.com>
2016-10-27 16:06:52 +0800

    shousi

--- configureStore.js
+++ configureStore.js
@@ -1,26 +1,33 @@
-import {createStore} from 'redux'
-import {saveState, getState} from './localStorage.js'
-import {throttle} from 'lodash'
-import visibleTodos from './reducers/index.js'
+import {createStore, applyMiddleware} from 'redux';
+import {saveState, getState} from './localStorage.js';
+import {throttle} from 'lodash';
+import visibleTodos from './reducers/index.js';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger'
 // var mockState = getState();
 // var store = createStore(visibleTodos);
-const addLoggerToDispatch = (store) => {
-	const rowDispatch = store.dispatch;
-	return (action) => {
-		const ref = rowDispatch(action);
-		return ref;
-	}
-}
-const addThunkToDispatch = (store) => {
-	const rowDispatch = store.dispatch;
-	return (action) => {
-		typeof action == 'function' ? action(store.dispatch) : rowDispatch(action);
-	}
-}
+// const addLoggerToDispatch = (store) => {
+// 	const next = store.dispatch;
+// 	return (action) => {
+// 		const ref = next(action);
+// 		return ref;
+// 	}
+// }
+// const addThunkToDispatch = (store, middlewares) => {
+// 	const next = store.dispatch;
+// 	return (action) => {
+// 		typeof action == 'function' ? action(store.dispatch) : next(action);
+// 	}
+// }
+// const wrapDispatch = (store, middlewares) => {
+// 	middlewares.forEach((middleware) => {
+// 		store.dispatch = middleware(store)
+// 	})
+// }
 const configureStore = () => {
-	var store = createStore(visibleTodos);
-	store.dispatch = addLoggerToDispatch(store);
-	store.dispatch = addThunkToDispatch(store);
+	// const middlewares = [addLoggerToDispatch, addThunkToDispatch];
+	let store = createStore(visibleTodos, applyMiddleware(thunk, createLogger()));
+	
 	return store;
 }
 
--- reducers/index.js
+++ reducers/index.js
@@ -1,45 +1,42 @@
-
-import {combineReducers} from 'redux'
-import todos, * as fromTodos from './todo.js';
-// const combineReducers = (reducers) => {
-//     return (state={}, action) => {
-//         var newState = {};
-//         Object.keys(reducers).filter((key) => {
-//             newState[key] = reducers[key](state[key], action)
-//         })
-//         return newState
-//     }
-// }
-// const combineReducers = (reducers) => {
-// 	return (state={}, action) => {
-// 		var newState = {};
-// 		Object.keys(reducers).map((key) => {
-// 			newState[key] = reducers[key](state[key], action)
-			
-// 		})
-// 		return newState
-// 	}
-// }
-
-
-export const getVisibleTodos = (state, visibleFilter) => {
-    let todos = fromTodos.getAllTodos(state.todos);
-	switch(visibleFilter) {
-		case 'all':
-			return todos;
-		case 'completed':
-			return todos.filter((todo) => {
-				return !todo.active
-			});
-		case 'active':
-		return todos.filter((todo) => {
-			return todo.active
-		})
-		default: 
-			return new Error('unknow filter')
-	}
+import {combineReducers} from 'redux';
+const byId = (state={}, action) => {
+    switch(action.type) {
+        case 'add_todo':
+            let nextState = Object.assign({}, state);
+            nextState[action.id] = {
+                text: action.text,
+                id: action.id,
+                active: true,
+            }
+            return nextState;
+        case 'toggle_todo': 
+            state[action.id] = Object.assign({}, state[action.id], {active: !state[action.id]})
+            return state;
+        case 'receive_todo': 
+            nextState = {};
+            action.response.forEach((todo) => {
+                nextState[todo.id] = todo;
+            })
+            return nextState;
+        default:
+            return state; 
+    }
 }
-
-
-export default combineReducers({todos});
-
+const allIds = (state=[], action) => {
+    switch(action.type) {
+        case 'add_todo':
+            return [...state, action.id];
+        case 'receive_todo' :
+            return action.response.map((todo) => todo.id)
+        default:
+            return state; 
+    }
+}
+export default combineReducers({
+                    byId,
+                    allIds
+                });
+                
+export const getAllTodos = (state) => {
+    return state.allIds.map(id => state.byId[id]);
+}
